bdc02cca776f0d3219505efc131c25c3f9dc03a1
Erica4705 <erica.atos@outlook.com>
2020-10-08 23:41:36 -0400

    Placeholder medications scheduled in Calendar page

--- lib/src/bloc/App_bloc.dart
+++ lib/src/bloc/App_bloc.dart
@@ -1,13 +1,17 @@
 
 import 'prescription/prescription_bloc.dart';
+import 'calendar/calendar_bloc.dart';
 
 class AppBloc {
 
   AppBloc() {
     _myPrescriptions = MyPrescriptionBloc();
+    _myCalendarDates = MyCalendarBloc();
   }
   
   MyPrescriptionBloc _myPrescriptions;
+  MyCalendarBloc _myCalendarDates;
 
   MyPrescriptionBloc  get myPrescriptionsBloc  => _myPrescriptions;
+  MyCalendarBloc get myCalendarDatesBloc => _myCalendarDates;
 }
