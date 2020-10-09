d446a2cb2b8981e1f3ab31913d93c614380d0b56
Erica4705 <erica.atos@outlook.com>
2020-10-09 11:35:34 -0400

    Cleaned up calendar files

--- lib/src/pages/calendar/calendar_page.dart
+++ lib/src/pages/calendar/calendar_page.dart
@@ -1,3 +1,5 @@
+import 'package:farmasee/src/pages/calendar/meeting.dart';
+import 'package:farmasee/src/pages/calendar/meeting_data_source.dart';
 import 'package:flutter/material.dart';
 
 import 'package:farmasee/src/widgets/app_bar_widget.dart';
@@ -54,45 +56,4 @@ class MyCalendarPage extends StatelessWidget {
 
     return meetings;
   }
-}
-
-class MeetingDataSource extends CalendarDataSource {
-  MeetingDataSource(List<Meeting> source) {
-    appointments = source;
-  }
-
-  @override
-  DateTime getStartTime(int index) {
-    return appointments[index].from;
-  }
-
-  @override
-  DateTime getEndTime(int index) {
-    return appointments[index].to;
-  }
-
-  @override
-  String getSubject(int index) {
-    return appointments[index].eventName;
-  }
-
-  @override
-  Color getColor(int index) {
-    return appointments[index].background;
-  }
-
-  @override
-  bool isAllDay(int index) {
-    return appointments[index].isAllDay;
-  }
-}
-
-class Meeting {
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
-
-  String eventName;
-  DateTime from;
-  DateTime to;
-  Color background;
-  bool isAllDay;
 }
